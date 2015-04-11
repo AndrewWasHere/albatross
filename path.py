5bd71492028a69c5271d4810edcd3b819482bcbe
Andrew Lin <andrew.lin@ieee.org>
2015-04-11 11:06:20 -0700

    Path utilities.

---
+++ path.py
@@ -0,0 +1,35 @@
+"""
+Copyright 2015 Andrew Lin.
+All rights reserved.
+Licensed under the BSD 3-clause License. See LICENSE.txt or
+<http://opensource.org/licenses/BSD-3-Clause>. 
+"""
+import os
+
+
+def abspath(path, start=None):
+    """The absolute path.
+
+    Expands user constructs and converts relative paths to absolute ones.
+
+    Args:
+        path (str): The path to expand.
+        start (str or None): Absolute path to start relative expansion from
+            (Can be a path to file or directory). Note: relative paths can
+            give unexpected results.
+            None => relative to current directory of running project.
+
+    Returns:
+        path (str): Absolute path.
+
+    Raises:
+        AttributeError: path and/or start is not a string-like object.
+    """
+    def dirname(p):
+        return os.path.dirname(p) if os.path.isfile(p) else p
+
+    path = os.path.expanduser(path)
+    path = os.path.abspath(
+        os.path.join(dirname(start), path) if start else path
+    )
+    return path
---
+++ test/test_path.py
@@ -0,0 +1,18 @@
+"""
+Copyright 2015 Andrew Lin.
+All rights reserved.
+Licensed under the BSD 3-clause License. See LICENSE.txt or
+<http://opensource.org/licenses/BSD-3-Clause>.
+"""
+import unittest
+
+import path
+
+
+class AbspathTestCase(unittest.TestCase):
+    def test_absolute(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
